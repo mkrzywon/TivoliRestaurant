c7326bed3097eb27de131cd10647cd87dcb59087
Marcin Krzywoń <mkrzywon@companer.pl>
2018-02-17 22:16:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/tivolirestaurant/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.tivolirestaurant;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
